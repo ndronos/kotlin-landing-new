06f378fb2e95d5396f61a28dc10a166aa8453ef0
Leandro Leite <ndronos@gmail.com>
2017-07-11 00:12:31 -0300

    Link Iteration /index

--- src/helloObjectOriented.kt
+++ src/helloObjectOriented.kt
@@ -3,21 +3,21 @@
  * class
  */
 
-class Greeter(val name: String) {
+class Greeter(val name: String) {           //  Class with a primary constructor and a member function
 
     fun greet() {
-        println("Hello, $name")
+        println("Hello, $name")             //  String Interpolation to cut down ceremony.
     }
 
 }
 
 fun main(args: Array<String>) {
 
-//  Greeter(args[0]).greet()
+//  Greeter(args[0]).greet()                //  Note that there's no 'new' keyword used to create an object
     Greeter("Kotlina!").greet()
 }
 
-/* (CLI) No arguments?
+/* (CLI) No arguments? (args[0])
 Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 0
 	at HelloObjectOrientedKt.main(helloObjectOriented.kt:16)
  */

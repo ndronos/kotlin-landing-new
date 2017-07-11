eea5c40348ea33f08c02a2f3436eaea419864291
Leandro Leite <ndronos@gmail.com>
2017-07-11 19:11:50 -0300

    New Iteration

--- src/Hello.kt
+++ src/Hello.kt
@@ -10,4 +10,16 @@ fun main(args: Array<String>) {
     print("Welcome to Kotlin ")
     println(2017)
 
+    // tests print commands
+    print("print on")
+    print(" ")
+    print("the same line...")
+
+    print("\n") // new line
+    println("") // new line
+
+    println("print on")
+    println("new line")
+
+    println(10)
 }

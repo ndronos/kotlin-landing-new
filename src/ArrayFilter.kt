e0dad8d006faeae2217e45e6d69765be044340d9
Leandro Leite <ndronos@gmail.com>
2017-07-11 19:43:02 -0300

    New Iteration

--- src/ArrayFilter.kt
+++ src/ArrayFilter.kt
@@ -6,5 +6,17 @@
 fun main(args: Array<String>) {
     val numbers = arrayListOf<Int>(1, 2, 3, 4, 5, 6)
 
-    println("Even numbers are ${numbers.filter { it % 2 == 0} }")
+    println("Even numbers are ${ numbers.filter { it % 2 == 0 } } ")
+    // Improvement...
+    println("Odd  numbers are ${ numbers.filter { it % 2 == 1 } } ")
+
+    // Improve interpolation...
+    val even = numbers.filter { it % 2 == 0 }
+    val odd  = numbers.filter { it % 2 == 1 }
+
+    println("Even numbers are ${even} ")
+    println("Odd  numbers are ${odd}  ")
+
+    println("Even numbers are $even ")
+    println("Odd  numbers are $odd  ")
 }

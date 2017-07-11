bd199e0a67434744c1ef6e4767ef413876116824
Leandro Leite <ndronos@gmail.com>
2017-07-11 15:33:55 -0300

    Link Iteration

--- src/DataClass.kt
+++ src/DataClass.kt
@@ -3,8 +3,8 @@
  * data class
  */
 
-class Customer(val name: String, val email: String)    // Customer@abcd1234
-
+//   class Customer(val name: String, val email: String)    // Customer@abcd1234
+data class Customer(val name: String, val email: String)
 
 fun main(args: Array<String>) {
     val customer = Customer("John Smith", "john.smith@somewhere.com")

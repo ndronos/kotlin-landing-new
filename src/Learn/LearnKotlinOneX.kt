3d896fe8ad6f2a19663d11414426d620cd557d1d
Leandro Leite <ndronos@gmail.com>
2017-07-14 23:50:08 -0300

    Iterate: main, Array, String

--- src/Learn/LearnKotlinOneX.kt
+++ src/Learn/LearnKotlinOneX.kt
@@ -9,6 +9,8 @@
 //  Package directive doesn't match file location // Move file to ... >
 package Learn
 
+//  Entry point to a Kotlin program is a function named "main"
+//  The function is passed a "Array" containing any command-line arguments "args"
 fun main(args: Array<String>) {
     println("Hello, World!")        // "Hello" classic
 }

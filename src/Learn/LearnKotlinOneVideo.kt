564e4501dea254c67c8fb897587160d458094279
Leandro Leite <ndronos@gmail.com>
2017-08-06 16:48:42 -0300

    Iterate: Data Types, Int

--- src/Learn/LearnKotlinOneVideo.kt
+++ src/Learn/LearnKotlinOneVideo.kt
@@ -9,12 +9,23 @@ fun main(args: Array<String>) {
     println("Hello, world!")
 
 
-    // ----- Data Types, Variables
+    // ----- Data Types -----
+
+    // Variables
 
     val name = "Kotlin"         // Create a read only variable
     val creation = 2011
     var now = 2017              // Mutable (changeable) variable
     println(now - creation)
+
+    // Kotlin uses Type Inference, but you can define the type
+
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+
+    println("Biggest Int: " + bigInt)
+    println("Smallest Int " + smallInt)
+
 
 
 

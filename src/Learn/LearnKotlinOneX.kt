cbfa69accbb191fdbf36b4a83e38c3e86fe769ef
Leandro Leite <ndronos@gmail.com>
2017-08-06 00:54:34 -0300

    Iterate: Sequences, Fibonacci

--- src/Learn/LearnKotlinOneX.kt
+++ src/Learn/LearnKotlinOneX.kt
@@ -243,6 +243,24 @@ code \t zzz ...
     val x = fooSequence.take(10).toList()
     println(x)
 
+// Sequences    // using a sequence to generate Fibonacci numbers
+
+    fun fibonacciSequence(): Sequence<Long> {
+        var a = 0L
+        var b = 1L
+
+        fun next(): Long {
+            val result = a + b
+            a = b
+            b = result
+            return a
+        }
+
+        return generateSequence (::next)
+    }
+
+    val y = fibonacciSequence().take(10).toList()
+    println(y)
 
 }
 

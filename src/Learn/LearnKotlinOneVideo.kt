b507f37a88d58efd9b7f94957cb7b341ca4e5dc2
Leandro Leite <ndronos@gmail.com>
2017-08-06 16:36:05 -0300

    Iterate: Data Types, val var

--- src/Learn/LearnKotlinOneVideo.kt
+++ src/Learn/LearnKotlinOneVideo.kt
@@ -7,5 +7,16 @@ package Learn
 
 fun main(args: Array<String>) {
     println("Hello, world!")
+
+
+    // ----- Data Types, Variables
+
+    val name = "Kotlin"         // Create a read only variable
+    val creation = 2011
+    var now = 2017              // Mutable (changeable) variable
+    println(now - creation)
+
+
+
 }
 

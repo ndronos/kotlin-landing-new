772813ece2260b13b5bf2f15488b7cf5f6b64342
Leandro Leite <ndronos@gmail.com>
2017-08-06 17:28:02 -0300

    Iterate: Debug, var to val, immutable

--- src/Learn/LearnKotlinOneVideo.kt
+++ src/Learn/LearnKotlinOneVideo.kt
@@ -20,33 +20,33 @@ fun main(args: Array<String>) {
 
     // Kotlin uses Type Inference, but you can define the type
 
-    var bigInt: Int = Int.MAX_VALUE
-    var smallInt: Int = Int.MIN_VALUE
+    val bigInt: Int = Int.MAX_VALUE
+    val smallInt: Int = Int.MIN_VALUE
 
     println("Biggest Int: " + bigInt)
     println("Smallest Int: " + smallInt)
 
-    var bigLong: Long = Long.MAX_VALUE
-    var smallLong: Long = Long.MIN_VALUE
+    val bigLong: Long = Long.MAX_VALUE
+    val smallLong: Long = Long.MIN_VALUE
 
     println("Biggest Long: " + bigLong)
     println("Smallest Long: " + smallLong)
 
-    var bigDouble: Double = Double.MAX_VALUE
-    var smallDouble: Double = Double.MIN_VALUE
+    val bigDouble: Double = Double.MAX_VALUE
+    val smallDouble: Double = Double.MIN_VALUE
 
     println("Biggest Double: " + bigDouble)
     println("Smallest Double: " + smallDouble)
 
-    var bigFloat: Float = Float.MAX_VALUE
-    var smallFloat: Float = Float.MIN_VALUE
+    val bigFloat: Float = Float.MAX_VALUE
+    val smallFloat: Float = Float.MIN_VALUE
 
     println("Biggest Float: " + bigFloat)
     println("Smallest Float: " + smallFloat)
 
     // Doubles are normally precise to 15 digits
-    var dblNum1: Double = 1.11111111111111111111
-    var dblNum2: Double = 1.11111111111111111111
+    val dblNum1: Double = 1.11111111111111111111
+    val dblNum2: Double = 1.11111111111111111111
 
     println("Double Sum: " + (dblNum1 + dblNum2))
 
@@ -61,7 +61,7 @@ fun main(args: Array<String>) {
 
     // Char: characters are single quoted characters
 
-    var letterGrade: Char = 'A'
+    val letterGrade: Char = 'A'
 
     println("A is a Char: " + (letterGrade is Char))
 

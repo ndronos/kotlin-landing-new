f8cae4c5559c6d7d765003c838cedba3e41bebd7
Leandro Leite <ndronos@gmail.com>
2017-08-11 11:19:12 -0300

    Iterate: Interface, interface, class, constructor, override fun

--- src/Learn/LearnKotlinOneVideo.kt
+++ src/Learn/LearnKotlinOneVideo.kt
@@ -622,6 +622,20 @@ interface Flyable {
     fun fly(distMiles: Double): Unit
 }
 
+// We override flies in the constructor
+// to implement the interface we follow the
+// constructor parameters with a colon and the
+// interface name
+class Bird constructor(val name: String,
+                       override var flies: Boolean = true): Flyable {
+
+    // We must also override any methods in the interface
+    override fun fly (distMiles: Double): Unit {
+        if(flies) {
+            println("$name flies $distMiles miles")
+        }
+    }
+}
 
 
 
